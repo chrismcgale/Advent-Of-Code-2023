08cef3e5f7613062925561984cbaa291041c9c81
Christopher McGale <56483395+chrismcgale@users.noreply.github.com>
2023-12-11 10:57:23 -0500

    Day 7 part 2

--- camel.rs
+++ camel.rs
@@ -25,6 +25,12 @@ fn kind(hand: &String) -> i32 {
 
     let k = unique.keys().len();
 
+    let mut jk = 0;
+    match unique.get(&("J".chars().next().unwrap())) {
+        Some(amount) => jk = *amount,
+        None => {}
+    };
+
     // 5 of a kind
     if k == 1 {
         return 7;
@@ -34,9 +40,15 @@ fn kind(hand: &String) -> i32 {
     if k == 2 {
         for v in unique.values() {
             if *v == 4 {
+                if jk >= 1 {
+                    return 7;
+                }
                 return 6;
             }
             if *v == 3 {
+                if jk > 0 {
+                    return 7;
+                }
                 return 5;
             }
         }
@@ -46,9 +58,18 @@ fn kind(hand: &String) -> i32 {
     if k == 3 {
         for v in unique.values() {
             if *v == 3 {
+                if jk >= 1 {
+                    return 6;
+                }
                 return 4;
             }
             if *v == 2 {
+                if jk == 2 {
+                    return 6;
+                }
+                if jk == 1 {
+                    return 5;
+                }
                 return 3;
             }
         }
@@ -56,11 +77,17 @@ fn kind(hand: &String) -> i32 {
 
     // Pair
     if k == 4 {
+        if jk >= 1 {
+            return 4;
+        }
         return 2;
     }
 
     // High card
     if k == 5 {
+        if jk == 1 {
+            return 2;
+        }
         return 1;
     }
 
@@ -77,29 +104,33 @@ fn main() -> std::io::Result<()> {
         .map(|(cards, amount)| (cards.to_owned(), amount.parse::<i32>().unwrap()))
         .collect();
 
-    let card_compare: HashMap<&str, i32> = hashmap!["2" => 0, "3" => 1, "4" => 2, "5" => 3, "6" => 4, "7" => 5, "8" => 6, "9" => 7, "T" => 8, "J" => 9, "Q" => 10, "K" => 11, "A" => 12];
+    let card_compare: HashMap<&str, i32> = hashmap!["J" => -1, "2" => 0, "3" => 1, "4" => 2, "5" => 3, "6" => 4, "7" => 5, "8" => 6, "9" => 7, "T" => 8, "Q" => 10, "K" => 11, "A" => 12];
 
     bets.sort_by(|bet1, bet2| {
         let a = &(*bet1).0;
         let b = &(*bet2).0;
 
+        println!("{} {} {} {}", a, kind(a), b, kind(b));
+
         if kind(a) > kind(b) {
             return Ordering::Greater;
         };
 
-        if kind(a) == kind(b) {
-            for i in 0..5usize {
-                let first = &a[i..i + 1];
-                let sec = &b[i..i + 1];
-                if card_compare[first] > card_compare[sec] {
-                    return Ordering::Greater;
-                } else if card_compare.get(sec) > card_compare.get(first) {
-                    return Ordering::Less;
-                }
+        if kind(a) < kind(b) {
+            return Ordering::Less;
+        };
+
+        for i in 0..5usize {
+            let first = &a[i..i + 1];
+            let sec = &b[i..i + 1];
+            if card_compare[first] > card_compare[sec] {
+                return Ordering::Greater;
+            } else if card_compare.get(sec) > card_compare.get(first) {
+                return Ordering::Less;
             }
         }
 
-        return Ordering::Less;
+        return Ordering::Greater;
     });
 
     println!("{:?}", bets);
